967fbe74cce6f88e75c7cb7bf5be3e333e34317d
AndresGarciaEscalante <andres_ge@hotmail.es>
2019-05-09 02:28:14 +0100

    Extraction of the Templates of both images

--- disparityEstimation.m
+++ disparityEstimation.m
@@ -36,14 +36,38 @@ function disparityMap = disparityEstimation(imageLeft,imageRight)
     %Adding the image to the new matrix.
     biggerImgLeft(windowOffset+1:leftImageRows+windowOffset,windowOffset+1:leftImageColumns+windowOffset,:)= imgLeft(:,:,:);
     biggerImgRight(windowOffset+1:rightImageRows+windowOffset,windowOffset+1:rightImageColumns+windowOffset,:)= imgRight(:,:,:);
-    %Ploting Both Images
-    figure
-    imshow(biggerImgLeft)
-    figure
-    imshow(biggerImgRight)
+%     %Ploting Both Images (Report Material!!!!!!!)
+%     figure
+%     imshow(biggerImgLeft)
+%     figure
+%     imshow(biggerImgRight)
 
+    %Extract the template from the new matrix (Report Material!!!!!!!)
+    ta = biggerImgLeft(1:windowSize+1-1,1:windowSize+1-1,:);
+    [taFeature,tahogVisualization] = extractHOGFeatures(ta,'CellSize',[15 10]);
+    tb = biggerImgRight(1:windowSize+1-1,1:windowSize+1-1,:);
+    [tbFeature,tbhogVisualization] = extractHOGFeatures(tb,'CellSize',[15 10]);
+    figure('Name', "Templates Image Left and Image Right"); 
+    %Plotting the Template A
+    subplot(2,1,1);
+    imshow(ta); 
+    hold on;
+    plot(tahogVisualization);
+    rectangle('Position',[1, 1, 30, 30],'LineWidth',1, 'EdgeColor', 'r')
+    hold off
+    %Plotting the Template B
+    subplot(2,1,2);
+    imshow(tb); 
+    hold on;
+    plot(tbhogVisualization);
+    rectangle('Position',[1, 1, 30, 30],'LineWidth',1, 'EdgeColor', 'b')
+    hold off
+    disparityMap = tbFeature;
+    
+    
     %#########################TO DO ##############################
     %Apply the HOG in the image for only 1 row of the new matrix
+    %Observavtions: Maybe the Edges of the images should be different (White Black)
     %#############################################################
     
     
@@ -83,23 +107,38 @@ function disparityMap = disparityEstimation(imageLeft,imageRight)
 %     figure
 %     imshow(cut);
     
-    figure
-    imshow(imgRight);
-    %Apply Hog in the provided 
-    f= zeros(leftImageRows,leftImageColumns);
-    [featureVector,hogVisualization] = extractHOGFeatures(imgLeft,'CellSize',[10 10]); % 15 10 (6 per Window), and 10 10 (9 per window)
-    [featureVector1,hogVisualization1] = extractHOGFeatures(f,'CellSize',[10 10]); 
-    disparityMap = featureVector1;
-    disparityMap = max(disparityMap);
-    figure;
-%     imshow(imgLeft); 
-    imshow(f); 
-    hold on;
-    plot(hogVisualization1);
-%     plot(hogVisualization);
-    hold off
+
+
+
+
+
+
+
+
+%     figure
+%     imshow(imgRight);
+%     %Apply Hog in the provided 
+%     f= zeros(leftImageRows,leftImageColumns);
+%     [featureVector,hogVisualization] = extractHOGFeatures(imgLeft,'CellSize',[10 10]); % 15 10 (6 per Window), and 10 10 (9 per window)
+%     [featureVector1,hogVisualization1] = extractHOGFeatures(f,'CellSize',[10 10]); 
+%     disparityMap = featureVector1;
+%     disparityMap = max(disparityMap);
+%     figure;
+% %     imshow(imgLeft); 
+%     imshow(f); 
+%     hold on;
+%     plot(hogVisualization1);
+% %     plot(hogVisualization);
+%     hold off
     
-    disparityMap = imgLeft(:,:,:);
+
+
+
+
+
+
+
+    
     %Going to the previous folder
     cd ..\
     
