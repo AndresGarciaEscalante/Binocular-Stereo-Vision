67b959add9d20ef9555726755d2faab1feb76651
AndresGarciaEscalante <andres_ge@hotmail.es>
2019-05-09 01:24:59 +0100

    New image created to apply the window algorithmn

--- disparityEstimation.m
+++ disparityEstimation.m
@@ -5,6 +5,7 @@
 % dispariyEstimation: Given the names of both images (strings) that 
 % will be analysed, using the Hog method, it will return the disparity 
 % map (Matrix)
+
 function disparityMap = disparityEstimation(imageLeft,imageRight)
     % Seeking the name of the Images in the Folder ExampleImages.
     try
@@ -19,23 +20,55 @@ function disparityMap = disparityEstimation(imageLeft,imageRight)
         error('Please make sure that the images are inside the folder "ExampleImages"')
     end 
     
-    %Displaying the information
-    leftImageRows = size(imgLeft,1);
-    leftImageColumns = size(imgLeft,2);
-    disparityMap = zeros(leftImageRows,leftImageColumns);
-    % The window moving in all the image.
-    windowSize= 31;  % Value of the dimensions of the window (Rectangle)
-    windowOffset=15; % Offset needed to put the desired pixel in the middle
-    % Display the actual window size.
+    %Variables
+    windowSize = 31;  %Value of the dimensions of the window (Rectangle). IMPORTANT (Only Odd Numbers)
+    windowOffset =15; %Offset needed to put the desired pixel in the middle. IMPORTANT ((windowSize-1)/2)
+    leftImageRows = size(imgLeft,1);      %NumberOfRowsLeftImage 
+    leftImageColumns = size(imgLeft,2);   %NumberOfColumnsLeftImage
+    rightImageRows = size(imgRight,1);    %NumberOfRowsRightImage 
+    rightImageColumns = size(imgRight,2); %NumberOfColumnsRightImage
+    rightWindow = zeros(windowSize,windowSize);%Matrix (windowSize x windowSize)
+    leftWindow = zeros(windowSize,windowSize); %Matrix (windowSize x windowSize)
+    disparityMap = zeros(leftImageRows,leftImageColumns); %DM LeftImageSize
+    % New part creating a new matrix with more pixels (This Matrices must be uint8 type)
+    biggerImgLeft = uint8(zeros(leftImageRows+windowOffset*2,leftImageColumns+windowOffset*2,3));   % LeftImage + 2*WindowOffset in the edges  ********Maybe we only need 1 dimension and not 3******
+    biggerImgRight = uint8(zeros(rightImageRows+windowOffset*2,rightImageColumns+windowOffset*2,3));% RightImage + 2*WindowOffset in the edges ********Maybe we only need 1 dimension and not 3******
+    %Adding the image to the new matrix.
+    biggerImgLeft(windowOffset+1:leftImageRows+windowOffset,windowOffset+1:leftImageColumns+windowOffset,:)= imgLeft(:,:,:);
+    biggerImgRight(windowOffset+1:rightImageRows+windowOffset,windowOffset+1:rightImageColumns+windowOffset,:)= imgRight(:,:,:);
+    %Ploting Both Images
     figure
-    imshow(imgLeft);
-    hold on 
-    % Iteration Zone
-%     k=0;
-%     for i = 1: leftImageRows-1
-%         for j = 1: leftImageColumns-1
+    imshow(biggerImgLeft)
+    figure
+    imshow(biggerImgRight)
+
+    %#########################TO DO ##############################
+    %Apply the HOG in the image for only 1 row of the new matrix
+    %#############################################################
+    
+    
+%     ta = imgLeft(1:windowSize+1-1,1:windowSize+1-1,:); % Problem
+%     figure
+%     imshow(ta);
+    
+%     % Iteration Zone
+%     for i = 1: leftImageRows         % RowPositionOfTheCenterPointOfWindow (i)
+%         for j = 1: leftImageColumns  % ColumnPositionOfTheCenterPointOfWindow (j) 
+%             leftWindow = zeros(windowSize,windowSize); % Matrix (windowSize x windowSize)
+%             ta = imgLeft(i:windowSize+i-1,j:windowSize+j-1,:); % Problem
+%         end
+%     end
+
+%     % Iteration Zone
+%     for i = 1: leftImageRows-1         %(i <= leftImageRows-1)
+%         for j = 1: leftImageColumns-1  %(j <= leftImageColumns-1)
+%             ta = imgLeft(i:windowSize+i,j:windowSize+j,:);
+%             [taFeature] = extractHOGFeatures(ta,'CellSize',[15 10]);
 %             for windowPos = 1: leftImageColumns-1
-%                 k=k+1;
+%                 tb = imgRight(i:windowSize+i,windowPos:leftImageColumns,:);
+%                 [tbFeature] = extractHOGFeatures(tb,'CellSize',[15 10]);
+%                 %Eclidean Distance     
+%                 eD = sqrt(sum((taFeature(:)-tbFeature(:)).^2))
 %             end
 % %             if ((j == 1)&&(i == 1)) || ((j == 449)&&(i == 374))
 % %                 rectangle('Position',[j-windowOffset,i-windowOffset,windowSize,windowSize],'LineWidth',1, 'EdgeColor', 'r')
@@ -43,23 +76,6 @@ function disparityMap = disparityEstimation(imageLeft,imageRight)
 %         end
 %     end
       
-    % Iteration Zone
-    k=0;
-%     for i = 1: leftImageRows-1
-        for j = 1: leftImageColumns-1
-            for windowPos = 1: leftImageColumns-1
-                
- 
-            end
-        end
-%     end
-
-%     %Eclidean Distance
-     tA = imgLeft(1:31,1:31,:);
-     tB = imgRight(1:31,1:31,:);
-%     [featureVector1] = extractHOGFeatures(tA,'CellSize',[15 10]);
-%     [featureVector2] = extractHOGFeatures(tA,'CellSize',[15 10]);
-%     eD = sqrt(sum((featureVector1(:)-featureVector2(:)).^2))
 %     
 %     hold off
 %     % Extract the window's local position
@@ -82,164 +98,12 @@ function disparityMap = disparityEstimation(imageLeft,imageRight)
     plot(hogVisualization1);
 %     plot(hogVisualization);
     hold off
+    
+    disparityMap = imgLeft(:,:,:);
     %Going to the previous folder
     cd ..\
     
 end
 
-% function sum = sumOfNumbers(A,B)
-%     sum = A+B;
-% end
-
-
-% %% Task 4 
-% %% Extract the template face from the image and plot template
-% start_r = 76; 
-% start_c = 142;
-% sz = 32;
-% tmplt = data(start_r:start_r+sz-1, start_c:start_c+sz-1, 1);
-% figure('Name', "Template"); imagesc(tmplt); colormap(gray)
-% 
-% %% Task 5
-% %% Normalise image and tmplt by dividing by 255
-% %% Do Euclidean distance based-matching between the template and frame 1
-% %% Plot the matching surface as a 2D image and find the location that
-% %% corresponds to the min value
-% %% What do you observe?
-% 
-% img = double(data(:, :, 1))./255;
-% tmplt = double(tmplt)./255;
-% score = zeros(size(img, 1)-sz, size(img, 2)-sz);
-% for ii = 1:size(img, 1)-sz
-%     for jj = 1:size(img, 2)-sz
-%         tar = img(ii:ii+sz-1, jj:jj+sz-1);     
-%         score(ii, jj) = sum((tmplt(:) - tar(:)).^2);
-%     end 
-% end
-% figure('Name', "Distance (Task 5)"); imagesc(score); colorbar
-% [posx, posy] = find(score == min(min(score)));
-% best_match_ED = [posx posy]
-
-%% Task 6
-%% Now replace Euclidean Distance with cross-correlation based-matching between the template and frame 1
-%% See https://en.wikipedia.org/wiki/Cross-correlation
-%% Plot the correlation surface as a 2D image and find the location that
-%% corresponds to the max value
-%% What do you observe?
-
-% img = double(data(:, :, 1))./255;
-% tmplt = double(tmplt)./255;
-% score = zeros(size(img, 1)-sz, size(img, 2)-sz);
-% for ii = 1:size(img, 1)-sz
-%     for jj = 1:size(img, 2)-sz
-%         tar = img(ii:ii+sz-1, jj:jj+sz-1);
-%         score(ii, jj) = tmplt(:)'*tar(:);
-%     end 
-% end
-% figure('Name', "Distance (Task 6)"); imagesc(score); colorbar
-% [posx, posy] = find(score == max(max(score)));
-% best_match_Corr = [posx posy]
-% figure('Name', "Output (Task 6)"); imagesc(squeeze(data(:,:,1))); colormap(gray)
-% best_match_Corr = [posx posy];
-% hold on 
-% rectangle('Position',[posy, posx, 30, 30],'LineWidth',2, 'EdgeColor', 'g')
-% hold off
-
-%% Task 7
-%% Repeat the above but replace correlation with zero-normalised cross-correlation
-%% See https://en.wikipedia.org/wiki/Cross-correlation
-%% [To note, you can also use corrcoef() in place above, as this normalises for you also.]
-%% [ie. use c = corrcoef(tmplt,tar);]
-
-% img = double(data(:, :, 1))./255;
-% tmplt = double(tmplt)./255;
-% score = zeros(size(img, 1)-sz, size(img, 2)-sz);
-% tmplt1 = tmplt(:) - mean(tmplt(:)); 
-% tmplt1 = tmplt1./norm(tmplt1);
-% for ii = 1:size(img, 1)-sz
-%     for jj = 1:size(img, 2)-sz
-%         tar = img(ii:ii+sz-1, jj:jj+sz-1);
-%         tar = tar(:) - mean(tar(:)); 
-%         tar = tar./norm(tar);
-%         score(ii, jj) = tmplt1(:)'*tar(:);
-%     end 
-% end
-% [posx, posy] = find(score == max(max(score)));
-% figure('Name', "Distance (Task 7)"); imagesc(score); colorbar
-% figure('Name', "Output (Task 7)"); imagesc(squeeze(data(:,:,1))); colormap(gray)
-% best_match_Corr = [posx posy];
-% hold on 
-% rectangle('Position',[posy, posx, 30, 30],'LineWidth',2, 'EdgeColor', 'g')
-% hold off
-
-%% Task 8
-%% Do tracking without tmplt update
-%% What do you observe?
-
-% img = double(data(:, :, 1))./255;
-% tmplt = double(tmplt)./255;
-% score = zeros(size(img, 1)-sz, size(img, 2)-sz);
-% tmplt1 = tmplt(:) - mean(tmplt(:)); 
-% tmplt1 = tmplt1./norm(tmplt1);
-% for dd = 1:length(filenames)
-%     img = double(data(:,:,dd)); 
-%     for ii = 1:size(img, 1)-sz
-%         for jj = 1:size(img, 2)-sz
-%             tar = img(ii:ii+sz-1, jj:jj+sz-1);
-%             tar = tar(:) - mean(tar(:)); 
-%             tar = tar./norm(tar);
-%             score(ii, jj) = tmplt1(:)'*tar(:);
-%         end 
-%     end
-%     [posx, posy] = find(score == max(max(score)));
-%     figure; imagesc(img); colormap(gray)
-%     hold on 
-%     rectangle('Position',[posy, posx, 30, 30],'LineWidth',2, 'EdgeColor', 'r')
-%     hold off
-%     pause(0.5)
-%     close all
-% end
-
-%% Task 9
-%% Do tracking with tmplt update
-%% What do you observe?
-
-% img = double(data(:, :, 1))./255;
-% tmplt = double(tmplt)./255;
-% score = zeros(size(img, 1)-sz, size(img, 2)-sz);
-% tmplt1 = tmplt(:) - mean(tmplt(:)); 
-% tmplt1 = tmplt1./norm(tmplt1);
-% for dd = 1:length(filenames)
-%     img = double(data(:,:,dd)); 
-%     for ii = 1:size(img, 1)-sz
-%         for jj = 1:size(img, 2)-sz
-%             tar = img(ii:ii+sz-1, jj:jj+sz-1);
-%             tar = tar(:) - mean(tar(:)); 
-%             tar = tar./norm(tar);
-%             score(ii, jj) = tmplt1(:)'*tar(:);
-%         end 
-%     end
-%     [posx, posy] = find(score == max(max(score)));
-%     %Show tracking position:
-%     hold on 
-%     figure; imagesc(img); colormap(gray)
-%     rectangle('Position',[posy, posx, 30, 30],'LineWidth',2, 'EdgeColor', 'r')
-%     hold off
-%     
-%     
-%     % update template
-%     start_r = posx;
-%     start_c = posy;
-%     tmplt1 = data(start_r:start_r+sz-1, start_c:start_c+sz-1, dd);
-%     figure('Name', "Template (Task 9)"); imagesc(tmplt1); colormap(gray)
-%     
-%     tmplt1 = double(tmplt1);
-%     tmplt1 = tmplt1(:) - mean(tmplt1(:)); 
-%     tmplt1 = tmplt1./norm(tmplt1);
-%     
-%     pause(3) % BIG PAUSE - so you can see the template updating.
-%     close all
-%     
-% end
 
 
